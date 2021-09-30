ed156337ec87ed371549ef686102ecb8c8e228e7
SeniorDmitriy <dimaponomarenko443@gmail.com>
2021-09-30 20:08:13 +0300

    added header,first section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,15 @@
-window.onload = function(){
-  console.log('hello');
-} 
+window.onload = function () {
+
+  $('.first__slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    infinite: false,
+    arrows: true,
+    fade: true,
+    prevArrow: '<button type="button" class="slick-prev">Назад</button>',
+    nextArrow: '<button type="button" class="slick-next">Вперед</button>',
+    appendArrows: $('.first__arrows'),
+    appendDots: '.slick-list'
+  });
+}
--- gulpfile.js
+++ gulpfile.js
@@ -45,6 +45,8 @@ function styles() {
 
 function scripts() {
   return src([
+      'node_modules/jquery/dist/jquery.js',
+      'node_modules/slick-carousel/slick/slick.js',
       'node_modules/svgxuse/svgxuse.js',
       'app/js/main.js'
     ])
